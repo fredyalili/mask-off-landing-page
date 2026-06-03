a8acb8b31905c95446eb71fe3aff8b80637223a7
v0agent <it+v0agent@vercel.com>
2026-06-03 16:09:06 +0000

    feat: adjust opacity of decorative SVG shapes in hero component

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,22 +7,22 @@ export function Hero() {
     <section className="relative min-h-screen pt-24 pb-16 md:pt-32 md:pb-24 flex items-center overflow-hidden">
       {/* Decorative background with shape SVGs */}
       <div className="absolute inset-0 -z-10">
-        <div className="absolute top-20 left-10 w-32 h-40 md:w-48 md:h-60 opacity-50">
+        <div className="absolute top-20 left-10 w-32 h-40 md:w-48 md:h-60 opacity-70">
           <Image src="/shapes/shape-1.svg" alt="" fill className="object-contain invert" />
         </div>
-        <div className="absolute top-32 right-16 w-28 h-36 md:w-44 md:h-56 opacity-50">
+        <div className="absolute top-32 right-16 w-28 h-36 md:w-44 md:h-56 opacity-70">
           <Image src="/shapes/shape-2.svg" alt="" fill className="object-contain invert" />
         </div>
-        <div className="absolute bottom-40 left-1/4 w-24 h-32 md:w-40 md:h-52 opacity-45">
+        <div className="absolute bottom-40 left-1/4 w-24 h-32 md:w-40 md:h-52 opacity-60">
           <Image src="/shapes/shape-3.svg" alt="" fill className="object-contain invert" />
         </div>
-        <div className="absolute bottom-20 right-1/3 w-28 h-36 md:w-44 md:h-56 opacity-45">
+        <div className="absolute bottom-20 right-1/3 w-28 h-36 md:w-44 md:h-56 opacity-60">
           <Image src="/shapes/shape-4.svg" alt="" fill className="object-contain invert" />
         </div>
-        <div className="absolute top-1/2 left-8 w-20 h-28 md:w-36 md:h-48 opacity-40">
+        <div className="absolute top-1/2 left-8 w-20 h-28 md:w-36 md:h-48 opacity-55">
           <Image src="/shapes/shape-5.svg" alt="" fill className="object-contain invert" />
         </div>
-        <div className="absolute bottom-1/3 right-10 w-24 h-32 md:w-40 md:h-52 opacity-40">
+        <div className="absolute bottom-1/3 right-10 w-24 h-32 md:w-40 md:h-52 opacity-55">
           <Image src="/shapes/shape-6.svg" alt="" fill className="object-contain invert" />
         </div>
       </div>
